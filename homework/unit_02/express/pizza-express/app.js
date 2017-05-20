b142a8c8f62b823dea8f0b8690b6ebeb79a626fc
rolita1007 <rolita1007@gmail.com>
2017-05-20 12:50:11 -0400

    factored routed and added views

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -5,7 +5,22 @@ var express = require('express');
 //save an express module as 'app'
 var app     = express();
 // assigning 3000 as our port
+var hbs = require('hbs');
+
+var toppings = require ('/controllers/toppingsController.js');
+
 var port    = 3000;
+
+app.set("views engine", "hbs");
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+	res.send('Welcome to Pizza Express');
+});
+
+app.use('/controllers', toppingsController.js);
+
+
 
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
