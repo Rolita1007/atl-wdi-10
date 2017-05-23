368da249b1d11327dd8f3d49468ace574063adab
rolita1007 <rolita1007@gmail.com>
2017-05-23 13:11:25 -0400

    set up express server

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -5,20 +5,20 @@ var express = require('express');
 //save an express module as 'app'
 var app     = express();
 // assigning 3000 as our port
-var hbs = require('hbs');
+//var hbs = require('hbs');
 
-var toppings = require ('/controllers/toppingsController.js');
+//var toppings = require ('/controllers/toppingsController.js');
 
 var port    = 3000;
 
-app.set("views engine", "hbs");
-app.set('views', './views');
+// app.set("views engine", "hbs");
+// app.set('views', './views');
 
-app.get('/', function (req, res) {
-	res.send('Welcome to Pizza Express');
-});
+// app.get('/', function (req, res) {
+// 	res.send('Welcome to Pizza Express');
+// });
 
-app.use('/controllers', toppingsController.js);
+// app.use('/controllers', toppingsController.js);
 
 
 
@@ -28,47 +28,7 @@ app.listen(port, function(){
   console.log('LISTENING ON PORT ' + port);
   console.log("==========================")
 
-var express = require('express');
-var app = express();
-var hbs = require('hbs');
 
-app.set("view engine, hbs");
-app.set('views' , '/views');
-
-
-app.get('/', function (req, res) {
-	res.send('Welcome to Pizza Express!');
-
-app.get('/topping/:type' ,function (req, res) {
-	res.send('toppings');
-
-app.get('/order/:amount/:size', function (req, res) {
-	res.send('order');
-})
-})
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var port = 3000;
-
-app.listen(port, function () {
-	console.log('running on port ' + port);
 });
+
+
