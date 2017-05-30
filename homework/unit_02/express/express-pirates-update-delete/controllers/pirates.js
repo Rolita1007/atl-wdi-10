2cc157ea95030ddc6baeaf0bbe41b870d69dee41
rolita1007 <rolita1007@gmail.com>
2017-05-30 19:34:28 -0400

    created delete and update

--- homework/unit_02/express/express-pirates-update-delete/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-update-delete/controllers/pirates.js
@@ -37,14 +37,64 @@ router.get('/:id', function(req, res){
 //==============================
 // CREATE
 //==============================
+router.post('/', function(req, res){
+  var pirateUser = new Pirate({
+    name: req.body.name,
+    birthplace: req.body.birthplace,
+    death_year: req.body.death_year,
+    base: req.body.base,
+    nickname: req.body.nickname
+  });
 
+  user.save(function(err, pirateUser) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(pirateUser);
+    res.render('pirates/show', {
+      pirate: pirate
+    });
+  });
+});
 //==============================
 // UPDATE
 //==============================
+ router.post('/id:', function(req, res) {
+  User.findByIdAndUpdate(req.params.id,
+    name: req.body.name,
+    birthplace: req.body.birthplace,
+    death_year: req.body.death_year,
+    base: req.body.base,
+    nickname: req.body.nickname
+  ), {new: true})
+    .exec(function(err, user) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(user);
+      res.render('pirates/show', {
+        pirates: pirates
+      });
+    });
+});
 
 //==============================
 // DESTROY
 //==============================
+router.delete('/id:', function(req, res) {
+  User.findByIdAndRemove(req.params.id)
+  .exec(function(err, user) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log('User deleted');
+    resend("User deleted");
+  });
+});
+
 
 //==============================
 // EXPORTS
--- homework/unit_02/express/express-pirates-update-delete/server.js
+++ homework/unit_02/express/express-pirates-update-delete/server.js
@@ -6,6 +6,7 @@ var app = express();
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 var hbs = require('hbs');
+var methodOverride = require('method-override');
 
 
 //===========================
@@ -20,6 +21,8 @@ app.use(bodyParser.json());
 app.set("view engine", "hbs");
 app.set('views', './views');
 
+//methodOverride
+app.use(methodOverride('_method'));
 //===========================
 // CONTROLLERS
 //===========================
