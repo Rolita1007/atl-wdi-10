3249ab053237a07c68181d6730727d4477252af6
rolita1007 <rolita1007@gmail.com>
2017-05-31 12:39:33 -0400

    updated homwork

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,44 +1,53 @@
 var express = require("express");
 var router = express.Router();
-var Pirates = require('../models/pirates.js');
+var seededPirates = require('../models/pirates.js');
 //all routes for /pirate
-router.post('/', function(req, res){
-  var pirate = new Pirate({
-    name: req.body.name,
-    birthplace: req.body.birthplace,
-    death_year: req.body.death_year,
-    base: req.body.base,
-    nickname: req.body.nickname
+router.get('/', function(req, res){
+  // res.send('hey friend');
+  res.render('pirates/index', {
+    seededPirates: seededPirates
   });
-  pirate.save(function(err, pirate) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(pirate);
-    res.render('pirates/show', {
-      pirate: pirate
-    });
-  });
-});
-router.patch('/id:', function(req, res) {
-  Pirate.findByIdAndUpdate(req.params.id,
-    name: req.body.name,
-    birthplace: req.body.birthplace,
-    death_year: req.body.death_year,
-    base: req.body.base,
-    nickname: req.body.nickname
-  ), {new: true})
-    .exec(function(err, pirate) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(pirate);
-      res.render('pirates/show', {
-        pirate: pirate
-      });
-    });
-});
+})
+
+// router.post('/', function(req, res){
+//   var pirate = new Pirate({
+//     name: req.body.name,
+//     birthplace: req.body.birthplace,
+//     death_year: req.body.death_year,
+//     base: req.body.base,
+//     nickname: req.body.nickname
+//   });
+//   pirate.save(function(err, pirate) {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log(pirate);
+//     res.render('pirates/show', {
+//       pirate: pirate
+//     });
+//   });
+// });
+
+// router.patch('/id:', function(req, res) {
+//   Pirate.findByIdAndUpdate(req.params.id, {
+//     name: req.body.name,
+//     birthplace: req.body.birthplace,
+//     death_year: req.body.death_year,
+//     base: req.body.base,
+//     nickname: req.body.nickname
+//   }, {new: true})
+//     .exec(function(err, pirate) {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log(pirate);
+//       res.render('pirates/show', {
+//         pirate: pirate
+//       });
+//     });
+// });
+
 //exports
 module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -3,20 +3,26 @@ var app = express();
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 var hbs = require('hbs');
+var methodOverride = require('method-override');
 
 //this is for morgan
 app.use(logger("dev"));
 //these are for bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 //set handlebars as view engine
 app.set("view engine", "hbs");
 app.set('views', './views');
 
+app.use(methodOverride('_method'));
+
 //controllers for `/pirates` resource
 var pirateController = require('./controllers/pirates.js');
 app.use("/pirates", pirateController);
 
+app.get('/', function(req, res) {
+  res.send('hi');
+});
 
 app.listen(3000, function(req, res){
   console.log("listening");
