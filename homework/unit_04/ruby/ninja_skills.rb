3f64f286475bc74efc437efd18d928ff557e8c11
rolita1007 <rolita1007@gmail.com>
2017-06-18 17:02:28 -0400

    2.Nill skills

--- homework/unit_04/ruby/ninja_skills.rb
+++ homework/unit_04/ruby/ninja_skills.rb
@@ -1,10 +1,20 @@
 a = true
 b = false
 puts a == b
-puts a !=b
+puts a ! = b
 
 major = nil
 puts major
 puts "#{major} nil was here"
 print major
 puts major.class
+
+d = Fixnum
+puts d
+e = Integer
+puts e
+
+d = e
+puts d
+
+
