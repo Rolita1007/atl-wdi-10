76ad679149efb0a00dfc4239d90efa53456d171c
rolita1007 <rolita1007@gmail.com>
2017-06-18 17:19:41 -0400

    String skills

--- homework/unit_04/ruby/ninja_skills.rb
+++ homework/unit_04/ruby/ninja_skills.rb
@@ -1,14 +1,17 @@
+#Challenge 1
 a = true
 b = false
 puts a == b
 puts a ! = b
 
+#Challenge 2
 major = nil
 puts major
 puts "#{major} nil was here"
 print major
 puts major.class
 
+#Challenge 3
 d = Fixnum
 puts d
 e = Integer
@@ -16,5 +19,29 @@ puts e
 
 d = e
 puts d
+
+#Challenge 4
+christine = "Hi, I'm Christine"
+like_to = "long walks on the beach, kinda."
+puts "#{christine} and I like #{like_to}"
+
+g = "2"
+g_to_number = 2
+
+puts g_to_number + g.to_i
+
+#Challenge 5
+
+
+
+
+
+
+
+
+
+
+
+
 
 
